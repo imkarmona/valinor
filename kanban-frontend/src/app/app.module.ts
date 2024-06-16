37b503c5f44cc0c597a75915651567f02609ecc2
guilherme.carmona <guilherme.carmona@ancode.app>
2024-06-16 13:11:19 -0300

    feat: create basic components and routing

--- kanban-frontend/src/app/app.module.ts
+++ kanban-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ColumnComponent } from './Components/column/column.component';
+import { TaskComponent } from './Components/task/task.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColumnComponent,
+    TaskComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
